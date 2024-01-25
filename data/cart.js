05fe58430bbfc619e7295b7367891985cf7f6863
Pranjal Shinde <shindept2003@gmail.com>
2024-01-25 21:03:01 +0530

    Added local storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,18 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
 },{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1,
 }];
+}
 
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -24,9 +31,11 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+  saveToStorage();
 
 }
 
 export function removeFromCart(productId){
-  return cart.pop(productId);
+  cart.pop(productId);
+  saveToStorage();
 }
