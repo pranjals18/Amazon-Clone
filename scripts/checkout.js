620fbb16fa0dc6ad68d8acad373894483afd2a8e
Pranjal Shinde <shindept2003@gmail.com>
2024-01-25 14:09:33 +0530

    Formatted documents

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import {products} from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = '';
 
@@ -29,7 +30,7 @@ cart.forEach((cartItem) => {
                 ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                $${matchingProduct.priceCents/100}
+                $${formatCurrenc(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                 <span>
